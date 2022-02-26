953cf36ddbeb6f5bfef015ad827aada8f9084e2a
marcosATr <marcos.trannin@gmail.com>
2022-02-26 15:49:52 -0300

    text styles implemented

--- projects/pokedex-using-react-and-redux.md
+++ projects/pokedex-using-react-and-redux.md
@@ -2,24 +2,33 @@
 title: Pokedéx using React and Redux
 slug: pokedex-using-react-and-redux
 date: 22 Jan 2022
-featured: /images/pokedex.png
+featured: /images/presentation-pokedex.png
 description: Everyone has a pokedex project. I didn't, up until now. This one was made with Redux and React!
 stack: React, Redux, Styled Components
 ---
 
-![Pokedéx com React e Redux](/images/pokedex.png)
 
-# Pokedéx com React e Redux
+
+### Um projeto feito para praticar Redux e Styled Components. O projeto segue à risca os padrões do template e a responsividade. Inspirado pelo projeto da comunidade do Figma: [Pokedéx, por Emanuel Garcia](https://www.figma.com/community/file/893705420616737018).
 
 Um projeto feito para praticar Redux e Styled Components. O projeto segue à risca os padrões do template e a responsividade. Inspirado pelo projeto da comunidade do Figma: [Pokedéx, por Emanuel Garcia](https://www.figma.com/community/file/893705420616737018).
 
+> This is a very long quotation mark to improve text beauty and make things a little more spicy. I hope I can learn something new from doing this.
+
+1. This is a very long quotation mark to improve text beauty and make things a little more spicy. I hope I can learn something new from doing this.
+2. This is a very long quotation mark to improve text beauty and make things a little more spicy. I hope I can learn something new from doing this.
+3. This is a very long quotation mark to improve text beauty and make things a little more spicy. I hope I can learn something new from doing this.
+
+![Pokedéx com React e Redux](/images/pokedex.png)
 **Stack**: React, Redux, Styled Components.
 
 Link github: [https://github.com/marcosATr/pokedex-with-redux](https://github.com/marcosATr/pokedex-with-redux)
 
 Link deploy: [https://serene-wright-e2f86e.netlify.app/](https://serene-wright-e2f86e.netlify.app/)
 
-### Destaque
+# Destaques
+
+--------------
 
 Carrossel animado:
 
--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -4,19 +4,94 @@ import fs from "fs/promises";
 import { ParsedUrlQuery } from "querystring";
 import showdown from "showdown";
 import { Metadata } from "../../utils/previews";
-import { Text } from "@chakra-ui/react";
+import { Box, Container, Image, Tag, Text } from "@chakra-ui/react";
 import ReactHtmlParser from "react-html-parser";
 import { Previews } from "../../utils/previews";
+import styled from "@emotion/styled";
 
 interface IParams extends ParsedUrlQuery {
   slug: string;
 }
 
-function Projects({ metadata, html }:Previews) {
+const ArticleBox = styled(Box)`
+  hr {
+    border-color: #cccccc;
+    margin: 50px 0px 30px;
+    opacity: 0.1;
+  }
+  ,
+  img {
+    margin: 50px 0;
+  }
+  ,
+  blockquote {
+    border-left: 2px solid #c5e943;
+    margin: 30px 2rem;
+    p {
+      color: #c5e943;
+      font-size: 2.5rem;
+      font-weight: 300;
+      line-height: 1.2;
+      padding: 10px 50px 10px;
+    }
+  }
+  ,
+  ul,
+  ol {
+    margin: 30px 40px 30px 50px;
+    padding: 0px 30px 0px 30px;
+    li {
+      margin: 10px 0;
+      font-size: 1.6rem;
+      color: rgb(128, 128, 128);
+      font-weight: 400;
+      line-height: 31px;
+    }
+  }
+  ,
+  h1 {
+    color: #ffffff;
+    font-size: 4rem;
+    line-height: 54px;
+    margin: 30px 2rem;
+  }
+  ,
+  h3 {
+    font-size: 2.5rem;
+    font-weight: 300;
+    line-height: 1.2;
+    color: #ffffff;
+    margin: 30px 2rem;
+  }
+  ,
+  p {
+    font-size: 1.6rem;
+    color: rgb(128, 128, 128);
+    margin: 30px 2rem;
+    font-weight: 400;
+    line-height: 31px;
+  }
+`;
+
+function Projects({ metadata, html }: Previews) {
   return (
     <>
       <Nav />
-      <article>{ReactHtmlParser(html)}</article>
+      <Container maxW="container.xl" mt="150px" py={4}>
+        <Box position="relative" h="360px" w="100%">
+          <Image alt={metadata.description} src={metadata.featured} objectFit="cover" h="100%" w="100%" />
+        </Box>
+      </Container>
+      <Container maxW="container.lg">
+        <Tag m="1rem 2rem" fontSize="xl" backgroundColor="#c5e943" px={8} py={2} borderRadius="20px">
+          {metadata.date}
+        </Tag>
+        <ArticleBox>
+          <h1>{metadata.title}</h1>
+          <hr></hr>
+          {ReactHtmlParser(html)}
+        </ArticleBox>
+      </Container>
     </>
   );
 }
